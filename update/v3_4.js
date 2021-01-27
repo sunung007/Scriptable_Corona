27d53610b5302f9a68a82f8d40d842c5ccf11d54
Gofo <38201078+sunung007@users.noreply.github.com>
2021-01-27 14:59:28 +0900

    Update v3_4.js
    url change

--- update/v3_4.js
+++ update/v3_4.js
@@ -16,7 +16,7 @@ async function update() {
   console.log('업데이트를 시작합니다.')
   
   // file link
-  const url = 'https://raw.githubusercontent.com/sunung007/IosScriptable/main/KoreaCovidWidget/main.js'
+  const url = 'https://raw.githubusercontent.com/sunung007/Scriptable_Corona/main/update/v3_4.js'
   
   console.log('파일 로드 중...')
   
