a2a6a80bea565c715fa5de774f2f0f0b44339566
Gofo <38201078+sunung007@users.noreply.github.com>
2021-02-05 13:06:29 +0900

    Update main.js
    icon fix

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: briefcase-medical;
 // Part : user
 const userSetting = {
   // 버튼
